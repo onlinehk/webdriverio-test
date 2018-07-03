9ac2d52ea9a9000b08318674bb8caf0bd2349d06
Raymond Lau <onlinehk@gmail.com>
2018-07-04 02:07:14 +0800

    added download file for readfile.js

--- readfile.js
+++ readfile.js
@@ -1,10 +1,19 @@
+var http = require('http');
+var url = require("url");
+var path = require("path");
 var fs = require('fs');
 
 var downloadFile = function(filename){
     fs.readFile(filename, 'utf8', function(err, data) {
       if (err) throw err;
       var jsonObj = JSON.parse(data);
-      console.log(jsonObj.length);
+      jsonObj.forEach(function (item){
+        var parsed = url.parse(item);
+        var file = fs.createWriteStream('download/' + path.basename(parsed.pathname));
+        http.get(item, function (response) {
+          response.pipe(file);
+        });
+      });
   });
 }
 
